9b658511964af50f9dfd5eb80878f1d8f4118975
Griffin Arkilic <garkilic@gmail.com>
2026-02-17 16:47:57 -0800

    Remove invalid turbo config option

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  turbo: {
-    root: path.resolve(__dirname),
-  },
 };
 
 export default nextConfig;
